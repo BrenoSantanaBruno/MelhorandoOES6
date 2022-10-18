ec587a5b923d4fdf2a57bb9ade8167697e2641df
Breno Santana <brenosantanabruno@live.com>
2022-10-18 05:33:12 -0300

    Firts Update of day.

--- Maps & Sets/creationandAddingItems.js
+++ Maps & Sets/creationandAddingItems.js
@@ -1,14 +1,40 @@
 let cardAce = {
-    name: 'Ace os Spades' +
-        ''
+    name: 'Ace os Spades'
 };
 
 let cardKing = {
     name: 'King of clubs'
 };
 
+let cardQueen = {
+    name: 'Queen of clubs'
+};
+
 let deck = new Map();
 deck.set('as', cardAce);
-deck.set('ks', cardKing)
+deck.set('ks', cardKing);
+deck.set('qe', cardQueen);
 
-console.log(deck);
+for (key of deck.keys()) {
+    console.log(key);
+}
+
+for (value of deck.values()) {
+    // parseInt(value, 10);
+    // console.log(value.length);
+    console.log(value);
+}
+
+
+// console.log(deck.size);
+
+// deck.delete('as');
+
+// deck.clear()
+
+//
+// console.log(deck.size);
+
+// console.log(deck.get('qe'));
+
+// console.log(deck.keys());
