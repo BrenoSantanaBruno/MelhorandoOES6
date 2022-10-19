01c96d7350f38c10ec28eefe0884cee832f8515a
Breno Santana <brenosantanabruno@live.com>
2022-10-18 23:33:00 -0300

    update about objects and yours manipulation #2

--- Objects/Analyzing Objects with Reflect.LownKeys().js
+++ Objects/Analyzing Objects with Reflect.LownKeys().js
@@ -19,7 +19,20 @@ let mum = {
 
 let person = new Person('Max', 27);
 // Reflect.set(person, 'name', 'Anna', mum);
-
+Reflect.defineProperty(person, 'hobbies', {
+    value: ['Sports', 'Cooking'],
+    // writable: true,
+    enumerable: true,
+    configurable: true
+});
 // console.log(mum);
 // console.log(Reflect.get(person,'name', mum));
-console.log(Reflect.ownKeys(person));
+// console.log(Reflect.ownKeys(person));
+person.hobbies = ['nothing'];
+// delete person.age;
+
+console.log(person.age);
+
+Reflect.deleteProperty(person, 'age');
+console.log(person.age);
+console.log(person.hobbies);
