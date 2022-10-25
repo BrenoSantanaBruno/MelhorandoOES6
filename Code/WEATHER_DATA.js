b082279a6d9fa8dceb56a8019e8c67e039b7d527
Breno Santana <brenosantanabruno@live.com>
2022-10-25 04:23:57 -0300

    Release01

---
+++ Code/WEATHER_DATA.js
@@ -0,0 +1,18 @@
+export class WEATHER_DATA {
+    constructor(cityName, description) {
+        this.cityName = cityName;
+        this.description = description;
+        this.temperature = '';
+    }
+}
+
+export const WEATHER_PROXY_HANDLER = {
+    get: function(target, property) {
+        return Reflect.get(target, property);
+    },
+
+    set: function(target, property, value) {
+        const newValue = (value * 1.8 + 32).toFixed(2) + 'F.';
+        return Reflect.set(target, property, newValue);
+    }
+}
